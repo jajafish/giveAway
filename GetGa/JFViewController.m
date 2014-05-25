f0c37a5a645699b6c7a41626913026e6bc130721
Jared Fishman <jfish@gigbear.com>
2014-05-25 13:15:53 -0400

    Initial Commit

---
+++ GetGa/JFViewController.m
@@ -0,0 +1,29 @@
+//
+//  JFViewController.m
+//  GetGa
+//
+//  Created by Jared Fishman on 5/25/14.
+//  Copyright (c) 2014 Jared Fishman. All rights reserved.
+//
+
+#import "JFViewController.h"
+
+@interface JFViewController ()
+
+@end
+
+@implementation JFViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ GetGaTests/GetGaTests.m
@@ -0,0 +1,34 @@
+//
+//  GetGaTests.m
+//  GetGaTests
+//
+//  Created by Jared Fishman on 5/25/14.
+//  Copyright (c) 2014 Jared Fishman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GetGaTests : XCTestCase
+
+@end
+
+@implementation GetGaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
