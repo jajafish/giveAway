337718a6ba336afd0b9af7d8108ea054dcfedda5
Jared Fishman <jfish@gigbear.com>
2014-05-31 22:28:43 -0400

    successfully added fb login before global navigation begins

---
+++ GetGa/JFLoginVC.h
@@ -0,0 +1,13 @@
+//
+//  JFLoginVC.h
+//  GetGa
+//
+//  Created by Jared Fishman on 5/31/14.
+//  Copyright (c) 2014 Jared Fishman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JFLoginVC : UIViewController
+
+@end
