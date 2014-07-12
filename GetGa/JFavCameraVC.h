9f1c296cdb3b93c2bd67e2169a4a1de41860ed15
Jared Fishman <jfish@gigbear.com>
2014-07-11 23:01:20 -0400

    implemented av foundation

---
+++ GetGa/JFavCameraVC.h
@@ -0,0 +1,26 @@
+//
+//  JFavCameraVC.h
+//  GetGa
+//
+//  Created by Jared Fishman on 7/11/14.
+//  Copyright (c) 2014 Jared Fishman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreVideo/CoreVideo.h>
+#import <CoreMedia/CoreMedia.h>
+#import <AVFoundation/AVFoundation.h>
+#import <ImageIO/ImageIO.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface JFavCameraVC : UIViewController
+
+@property (nonatomic, retain) IBOutlet UIView *vImagePreview;
+
+@property (strong, nonatomic) IBOutlet UIButton *takePicButton;
+
+@property(nonatomic, retain) AVCaptureStillImageOutput *stillImageOutput;
+
+@property (strong, nonatomic) IBOutlet UIImageView *outputImageView;
+
+@end
